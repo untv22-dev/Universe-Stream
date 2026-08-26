a8a4ecb098c151ec181e22492b8ba430fe807a65
untv22-dev <2.54227075e+08+untv22-dev@users.noreply.github.com>
2026-08-26 02:42:39 +0000

    fix: add room paging runtime dependency

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     // Room
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
+    implementation(libs.room.paging)
     implementation(libs.paging.runtime)
     ksp(libs.room.compiler)
 
